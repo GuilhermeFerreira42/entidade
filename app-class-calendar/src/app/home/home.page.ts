0d37ee6fd28ceafc7a6fed8933a99f3b0b331eec
nicolasghcorreia <nicolas99correia@gmail.com>
2023-06-17 19:50:24 -0300

    criação grid aluno

--- app-class-calendar/src/app/home/home.page.ts
+++ app-class-calendar/src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IonicModule, NavController } from '@ionic/angular';
+import { BuscaDadosService } from '../api/busca-dados.service';
 
 @Component({
   selector: 'app-home',
@@ -13,11 +14,58 @@ export class HomePage {
 
   public usuario : any;
   public userType: any;
+  monitoriaType: any = 'monitoria'
+  horarioType: any = 'horario'
+  itens: any = [];
+  items: any = [];
+  idMonitoria: any;
+  estado: any;
+  userInput: any;
 
-  constructor(private navCtrl: NavController, private route : ActivatedRoute) {}
+  constructor(private navCtrl: NavController, private route : ActivatedRoute, private service : BuscaDadosService) {}
+
+  public getAlldados (){
+    this.service.getAllMonitoria(this.monitoriaType+'s').then(dados=>{
+      this.items=dados;
+    })
+
+  }
+
+  public getAllhorario (){
+    this.service.getAllHorario(this.horarioType+'s').then(dados=>{
+      this.itens=dados;
+    })
+    
+  }
+
+  public verificarIdAluno(items: any[], usuario: any):boolean {
+    for(let i = 0; i < items.length; i++){
+      if (items[i].aluno.IdAluno===usuario.IdAluno){
+        return true;
+      } 
+    }
+    return false;
+  }
+
+  public todosMonitoriasIndisponiveis(items:any[]):boolean {
+    return this.items.every((items:{estado: any;})=>items.estado===false);
+  }
+
+  public checkHorarioDisponivel(itens:any[]):boolean{
+    return itens.some(horario=>horario.disponivel==false);
+  }
+
+  formatarHorario(horarioNumerico: number): string {
+    const horarioString = horarioNumerico.toString();
+    const hora = horarioString.substring(0, horarioString.length - 2);
+    const minutos = horarioString.substring(horarioString.length - 2);
+    return hora + ':' + minutos;
+  }
 
   ngOnInit() {
-    this.route.queryParams.subscribe(params => {
+      this.getAlldados()
+      this.getAllhorario()
+      this.route.queryParams.subscribe(params => {
       this.usuario = params['usuario'];
       this.userType = params['userType']});
   }
