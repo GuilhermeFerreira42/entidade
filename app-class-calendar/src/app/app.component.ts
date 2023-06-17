08145f52d976546fe5bc201231ecc53d54f9bdf9
ArthurBarbatodaSilva <barbatui0311@gmail.com>
2023-06-17 17:02:36 -0300

    Slide

--- app-class-calendar/src/app/app.component.ts
+++ app-class-calendar/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
-import { NavController } from '@ionic/angular';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AlertController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -8,74 +8,94 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-
+  
   public usuario:any
   public userType:any
-  constructor(private router: Router,private navCtrl: NavController,private route: ActivatedRoute) {}
 
-  exibirMenu(): boolean{
+  constructor(private alertController: AlertController, private navCtrl: NavController, private route: ActivatedRoute, private router: Router) { }
+
+  goCadastro(){
+    this.navCtrl.navigateForward('cadastro', {
+      queryParams: { usuario: this.usuario,
+                    userType: this.userType}
+    });
+  }
+
+  goAlunos(){
+    this.navCtrl.navigateForward('dados-aluno', {
+      queryParams: { usuario: this.usuario,
+                    userType: this.userType}
+    });
+  }
+
+  goTecnicos(){
+    this.navCtrl.navigateForward('dados-tecnico', {
+      queryParams: { usuario: this.usuario,
+                    userType: this.userType}
+    });
+  }
+
+  goProfessores() {
+    this.navCtrl.navigateForward('dados-professor', {
+      queryParams: { button: 'professores',
+                    usuario: this.usuario,
+                    userType: this.userType }
+    })
+  }
+
+  goDisciplinas() {
+    this.navCtrl.navigateForward('dados-professor', {
+      queryParams: { button: 'disciplinas',
+                    usuario: this.usuario,
+                    userType: this.userType }
+    })
+  }
+
+  goHorario() {
+    this.navCtrl.navigateForward('horario', {
+      queryParams: { usuario: this.usuario,
+                    userType: this.userType }
+    })
+  }
+
+  async goExit() {
+    const alert = await this.alertController.create({
+      header: 'Confirmação',
+      message: 'Tem certeza que deseja sair?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+        }, {
+          text: 'Sair',
+          handler: () => {
+            this.navCtrl.navigateForward('login')
+          }
+        }
+      ]
+    });
+      await alert.present();
+  }
+
+  exibirMenu(): boolean {
     const excludedRoutes = ['login']; // Adicione as rotas que devem excluir o menu
-    
+    const excludedUserTypes = ['aluno', 'professor']; // Adicione os tipos de usuário que devem excluir o menu
+  
     // Verifique se a rota atual está na lista de rotas excluídas
     const currentRoute = this.router.url.split('/')[1];
-    return !excludedRoutes.includes(currentRoute);
-}
+    const userType = this.getCurrentUserType(); // Função para obter o userType atual
+  
+    return !excludedRoutes.includes(currentRoute) && !excludedUserTypes.includes(userType);
+  }
 
-goCadastro(){
-  this.navCtrl.navigateForward('cadastro', {
-    queryParams: { usuario: this.usuario,
-                  userType: this.userType}
-  });
-}
 
-goAlunos(){
-  this.navCtrl.navigateForward('aluno', {
-    queryParams: { usuario: this.usuario,
-                  userType: this.userType}
-  });
-}
+  getCurrentUserType(): string {
+    return this.userType
+  }
 
-goTecnicos(){
-  this.navCtrl.navigateForward('tecnico', {
-    queryParams: { usuario: this.usuario,
-                  userType: this.userType}
-  });
-}
-
-goProfessores() {
-  this.navCtrl.navigateForward('professor', {
-    queryParams: { button: 'professores',
-                  usuario: this.usuario,
-                  userType: this.userType }
-  })
-}
-
-goDisciplinas() {
-  this.navCtrl.navigateForward('disciplina', {
-    queryParams: {
-                  usuario: this.usuario,
-                  userType: this.userType }
-  })
-}
-
-goHorario() {
-  this.navCtrl.navigateForward('horario', {
-    queryParams: { usuario: this.usuario,
-                  userType: this.userType }
-  })
-}
-
-goHome(){
-  this.navCtrl.navigateForward('home', {
-    queryParams: { usuario: this.usuario,
-                  userType: this.userType}
-  });
-}
-
-ngOnInit() {
-  this.route.queryParams.subscribe(params => {
-    this.usuario = params['usuario'];
-    this.userType = params['userType']});
-}
-
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.usuario = params['usuario'];
+      this.userType = params['userType']});
+  }
 }
