c4c3a141877b1b443414f8a532863a2613fc0f59
ArthurBarbatodaSilva <barbatui0311@gmail.com>
2023-06-17 14:55:56 -0300

    Horario

--- app-class-calendar/src/app/horario/horario.page.ts
+++ app-class-calendar/src/app/horario/horario.page.ts
@@ -1,4 +1,7 @@
+import { NavController } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
+import { DadosService } from '../api/dados.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-horario',
@@ -7,9 +10,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HorarioPage implements OnInit {
 
-  constructor() { }
+  materias:any = [];
+  public usuario: any;
+  public userType: any;
+  disciplinaType:any = 'disciplinas';
+  userGroup: any = 'professores';
+  itens:any = []
+
+  constructor(private route: ActivatedRoute, private serviceMaterias: DadosService, private service: DadosService, private navCtrl: NavController) { }
+
+  public getAll(){
+    this.serviceMaterias.getAll(this.disciplinaType).then(dados => {
+      this.materias = dados;
+      console.log(this.materias)
+    })
+  }
+
+  public getAllDados(){
+    this.service.getAllDados(this.userGroup).then(dados => {
+      this.itens = dados;
+      console.log(this.itens)
+    })
+  }
+
+  public irHorarioAgendamento(materia:any){
+    this.navCtrl.navigateForward('materia', {
+      queryParams: {materia: materia,
+                    usuario: this.usuario,
+                    userType: this.userType }
+    });
+  }
+
+  goPerfil(){
+    this.navCtrl.navigateForward('perfil', {
+      queryParams: { usuario: this.usuario,
+                     userType: this.userType }
+    });
+  }
+
+  goHome(){
+    this.navCtrl.navigateForward('home', {
+      queryParams: { usuario: this.usuario,
+                     userType: this.userType }
+    });
+  }
 
   ngOnInit() {
+    this.getAll();
+    this.getAllDados();
+    this.route.queryParams.subscribe(params => {
+      this.usuario = params['usuario'];
+      this.userType = params['userType']});
   }
 
 }
