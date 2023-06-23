8e51fe41cf54c98b1dbb511023b94be681c846d5
DanielSouzaCruz <danielrenovar165@gmail.com>
2023-06-22 21:59:55 -0300

    Trabalho apresentado

--- app-class-calendar/src/app/api/busca-dados.service.ts
+++ app-class-calendar/src/app/api/busca-dados.service.ts
@@ -6,11 +6,9 @@ import { Injectable } from '@angular/core';
 })
 export class BuscaDadosService {
 
-    public host: string = 'http://localhost:8080/api/'
+  public host: string = 'http://localhost:8080/api/'
 
   constructor(private http: HttpClient) {  }
-
-
 
   public getAllDados(userType:any){
     return new Promise((ret) => {
